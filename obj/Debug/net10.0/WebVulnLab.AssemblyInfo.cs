005786be52068f5cf2fddc1e0706ddcf37b9fae4
Ahmet <afsecure256@gmail.com>
2026-05-16 20:23:06 +0300

    Minor changes

--- obj/Debug/net10.0/WebVulnLab.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebVulnLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebVulnLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7931a70ecbc66c25fc946fe2eeda698c7405e357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e3cdb28b428bcc754722bdda5e02d0f47b64d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebVulnLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebVulnLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
